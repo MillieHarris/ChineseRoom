a4ff74bb19b1bf55e67b4681e05f399f1c175d86
MillieHarris <msharris17@gmail.com>
2020-03-29 13:57:42 -0400

    Latest version of unity project with room scene and book scene, including scripts for basic movement and sprite assets

---
+++ CRE_Unity/Chinese Room Experiment/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    float speed = 6;
+    float rotationSpeed = 80;
+    float gravity = 8;
+    float rot = 0f;
+    public Rigidbody character;
+    public Animator bookanim;
+
+    Vector3 moveDir = Vector3.zero;
+
+    CharacterController controller;
+    Animator anim;
+    public bool isGrounded;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+
+
+
+    void OnTriggerEnter(Collider collision)
+    {
+        print("test");
+        controller.transform.position = new Vector3(-219, -328, 54);
+        anim.SetInteger("condition", 3);
+        bookanim.SetInteger("bookcondition", 1);
+
+
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        anim.SetInteger("condition", 5);
+    }
+
+
+    //   void OnCollisionEnter(Collision collision)
+    //   {
+    //     print("test");
+
+    //    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && (anim.GetInteger("condition") == 3))
+        {
+            print("hello");
+           // anim.SetInteger("condition", 2);
+            controller.transform.position = new Vector3(-183, -272, 310);
+                
+        }
+
+        // if (controller.isGrounded)
+        {
+
+            if (Input.GetKey(KeyCode.W) && (anim.GetInteger("condition") != 3))
+            {
+                moveDir = new Vector3(0, 0, 1);
+                moveDir *= speed;
+                moveDir = transform.TransformDirection(moveDir);
+                controller.transform.Translate(Vector3.forward);
+                anim.SetInteger("condition", 1);
+
+            }
+
+            if (Input.GetKeyUp(KeyCode.W) && (anim.GetInteger("condition") != 3))
+            {
+                moveDir = new Vector3(0, 0, 0);
+                moveDir *= speed;
+                anim.SetInteger("condition", 2);
+            }
+
+            rot += Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+            transform.eulerAngles = new Vector3(0, rot, 0);
+            
+
+            moveDir.y -= gravity * Time.deltaTime;
+
+        }
+    }
+}
+
