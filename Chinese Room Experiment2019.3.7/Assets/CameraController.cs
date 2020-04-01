5a84236476c334b18cd72ac8a53fc45a7a72f5fa
MillieHarris <msharris17@gmail.com>
2020-03-31 21:51:29 -0400

    resubmitting new version of project
    2019.3.7 updated version of project with the new ui features (sticking with this version of unity so commits will be compatible and new projects will not be needed to upload each time)

---
+++ Chinese Room Experiment2019.3.7/Assets/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject target;
+    Vector3 offset;
+
+    void LateUpdate()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = target.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Chinese Room Experiment2019.3.7/Assets/NotePickup.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotePickup : MonoBehaviour
+{
+    [SerializeField] private Image customImage;
+    [SerializeField] private Image customImage2;
+    [SerializeField] private Button customButton;
+    [SerializeField] private Text customText;
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if( other.CompareTag("Player") && gameObject.tag == "Shelf")
+        { customImage.enabled = true;
+            print("helooooo");
+            customImage2.enabled = true;
+            //customButton.enabled = true;
+           // customText.enabled = true;
+        }
+    }
+
+    public void BackButton()
+    { customImage.enabled = false;
+        customImage2.enabled = false;
+        customButton.enabled = false;
+        customText.enabled = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            customImage.enabled = false;
+            customImage2.enabled = false;
+        }
+    }
+}
