774da4d3e48511776d5bf599499060e94fdb649d
MillieHarris <48485248+MillieHarris@users.noreply.github.com>
2020-04-08 23:40:11 -0400

    book pro scene and scripts for output population on click

---
+++ Chinese Room Experiment2019.3.7/Assets/PopulateOutput.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulateOutput : MonoBehaviour
+{
+
+    public GameObject finalOutput;
+    public GameObject X;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OutputClick()
+    {
+
+        finalOutput.SetActive(true);
+
+    }
+
+    public void WrongClickFunc()
+    { StartCoroutine("WrongClick"); }
+
+      public IEnumerator WrongClick()
+         {
+              X.SetActive(true);
+              yield return new WaitForSeconds(1);
+              X.SetActive(false);
+
+          }
+}
